a24f6fed8c434811ff69e4951dd4fd8eccda9319
Enfield-Li <1076462153@qq.com>
2022-05-07 20:54:04 +0800

    Testing

--- src/main/java/com/example/reddit/post/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.reddit.post;
-
-import com.example.reddit.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {}
