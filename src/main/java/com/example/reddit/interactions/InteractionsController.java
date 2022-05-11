fdbd7cad6f9f400ce15a5f8228a9e57fc5b90b62
Enfield-Li <1076462153@qq.com>
2022-05-12 05:17:33 +0800

    Interaction setup

--- src/main/java/com/example/reddit/interactions/InteractionsController.java
+++ src/main/java/com/example/reddit/interactions/InteractionsController.java
@@ -4,21 +4,18 @@ import com.example.reddit.interactions.entity.Interactions;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Interactions")
@@ -44,18 +41,42 @@ class InteractionsController {
   }
 
   @PatchMapping("setNotificationChecked")
-  public ResponseEntity<Boolean> setNotificationChecked() {
-    return null;
+  public ResponseEntity setNotificationChecked(HttpSession session) {
+    try {
+      Long meId = (Long) session.getAttribute("userId");
+
+      return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    } catch (Exception e) {
+      return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Something's gone wrong...");
+    }
   }
 
   @PatchMapping("setInteractionRead")
-  public ResponseEntity<Boolean> setInteractionRead() {
-    return null;
+  public ResponseEntity setInteractionRead(HttpSession session) {
+    try {
+      Long meId = (Long) session.getAttribute("userId");
+
+      return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    } catch (Exception e) {
+      return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Something's gone wrong...");
+    }
   }
 
   @PatchMapping("setAllInteractionRead")
-  public ResponseEntity<Boolean> setAllInteractionRead() {
-    return null;
+  public ResponseEntity setAllInteractionRead(HttpSession session) {
+    try {
+      Long meId = (Long) session.getAttribute("userId");
+
+      return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    } catch (Exception e) {
+      return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Something's gone wrong...");
+    }
   }
 
   @GetMapping("interact/post/{id}")
@@ -95,20 +116,32 @@ class InteractionsController {
   }
 
   @GetMapping("interactives")
-  public ResponseEntity<Boolean> create(@RequestBody Interactions item) {
-    return null;
-  }
+  public ResponseEntity create(HttpSession session) {
+    try {
+      Long meId = (Long) session.getAttribute("userId");
 
-  @PutMapping("{id}")
-  public ResponseEntity<Boolean> update(
-    @PathVariable("id") Long id,
-    @RequestBody Interactions item
-  ) {
-    return null;
+      return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    } catch (Exception e) {
+      return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Something's gone wrong...");
+    }
   }
 
   @GetMapping("interact/comment/{id}")
-  public ResponseEntity<Boolean> delete(@PathVariable("id") Long id) {
-    return null;
+  public ResponseEntity delete(
+    @PathVariable("id") Long id,
+    HttpSession session,
+    @RequestParam(name = "value", required = true) Boolean value
+  ) {
+    try {
+      Long meId = (Long) session.getAttribute("userId");
+
+      return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    } catch (Exception e) {
+      return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Something's gone wrong...");
+    }
   }
 }
