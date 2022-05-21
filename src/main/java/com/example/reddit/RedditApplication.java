aa3dcf5305b6c0a540a6151a6670ca500070fda5
Enfield-Li <1076462153@qq.com>
2022-05-22 03:32:25 +0800

    Set up

--- src/main/java/com/example/reddit/RedditApplication.java
+++ src/main/java/com/example/reddit/RedditApplication.java
@@ -1,14 +1,55 @@
 package com.example.reddit;
 
-import java.util.Arrays;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class RedditApplication {
+@SpringBootApplication
+public class RedditApplication implements CommandLineRunner {
+
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired
+  private EmployeeMyBatisRepository employeeRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+    logger.info(
+      "Inserting -> {}",
+      employeeRepository.insert(
+        new Employee(10011L, "Ramesh", "Fadatare", "ramesh@gmail.com")
+      )
+    );
+    logger.info(
+      "Inserting -> {}",
+      employeeRepository.insert(
+        new Employee(10012L, "John", "Cena", "john@gmail.com")
+      )
+    );
+    logger.info(
+      "Inserting -> {}",
+      employeeRepository.insert(
+        new Employee(10013L, "tony", "stark", "stark@gmail.com")
+      )
+    );
+
+    logger.info("Employee id 10011 -> {}", employeeRepository.findById(10011L));
+
+    logger.info(
+      "Update 10003 -> {}",
+      employeeRepository.update(
+        new Employee(10011L, "ram", "Stark", "ramesh123@gmail.com")
+      )
+    );
+
+    employeeRepository.deleteById(10013L);
+
+    logger.info("All users -> {}", employeeRepository.findAll());
+  }
 
   public static void main(String[] args) {
     ConfigurableApplicationContext configContext = SpringApplication.run(
